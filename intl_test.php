6b7fdb0914dab38f9d55c88b5fffee8656c05e6c
chetantarale <chetan@shippable.com>
2016-03-02 13:43:07 +0530

    Update intl_test.php

--- intl_test.php
+++ intl_test.php
@@ -1,10 +1,6 @@
 <?php
 class intl_test {
   public static function main() {
-    $coll = new Collator('en_US');
-    $al   = $coll->getLocale(Locale::ACTUAL_LOCALE);
-    echo "Actual locale: $al\n";
-    
     $formatter = new NumberFormatter('en_US', NumberFormatter::DECIMAL);
     return $formatter->format(1234567);
   }
