da9f779956a46331651259e509079b3f2b5b464d
chetantarale <chetan@shippable.com>
2016-03-02 12:53:41 +0530

    Update intl_test.php

--- intl_test.php
+++ intl_test.php
@@ -1,15 +1,12 @@
 <?php
 class intl_test {
   public static function main() {
-    $DateTime = new DateTime();
-    $IntlDateFormatter = new IntlDateFormatter(
-      'es_ES@calendar=islamic',
-      IntlDateFormatter::FULL,
-      IntlDateFormatter::FULL,
-      'Australia/Yancowinna',
-      IntlDateFormatter::TRADITIONAL
-    );
-  return $IntlDateFormatter->format($DateTime);
+    $coll = new Collator('en_US');
+    $al   = $coll->getLocale(Locale::ACTUAL_LOCALE);
+    echo "Actual locale: $al\n";
+    
+    $formatter = new NumberFormatter('en_US', NumberFormatter::DECIMAL);
+    return $formatter->format(1234567);
   }
 }
 ?>
